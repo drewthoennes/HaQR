bb4b880338a26e599fdfa8c0d802939c78472ef4
drewthoennes <dthoenne@purdue.edu>
2019-06-25 18:41:31 -0500

    Styles containers and optimizes for mobile

--- src/frontend/containers/HackersPage/index.js
+++ src/frontend/containers/HackersPage/index.js
@@ -15,10 +15,12 @@ class HackersPage extends React.Component {
 
     this.state = {
       hackers: [],
+      search: '',
       loaded: false,
       showScanner: false
     };
 
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.showScanner = this.showScanner.bind(this);
     this.hideScanner = this.hideScanner.bind(this);
     this.getHackers = this.getHackers.bind(this);
@@ -28,6 +30,10 @@ class HackersPage extends React.Component {
   componentDidMount() {
     console.log(this.props.account.token);
     this.getHackers();
+  }
+
+  onSearchChange(event) {
+    this.setState({search: event.target.value});
   }
 
   getHackers() {
@@ -68,8 +74,11 @@ class HackersPage extends React.Component {
   }
 
   render() {
-    let hackers = this.state.hackers.map(hacker => (
-      <p key={hacker.qr}>{hacker.name} ({hacker.email})</p>
+    let search = this.state.search.toLowerCase();
+    let hackers = this.state.hackers.filter(hacker => {
+      return hacker ? hacker.name.toLowerCase().includes(search) : false;
+    }).map(hacker => (
+      <button className="list-group-item" key={hacker.qr}>{hacker.name} ({hacker.email})</button>
     ));
 
     let scanner = '';
@@ -79,10 +88,17 @@ class HackersPage extends React.Component {
 
     return (
       <div id="hackersPage" className="tall">
-        <div className={`content tall ${this.state.showScanner ? 'blur' : ''}`}>
-          {hackers}
-          <button onClick={this.showScanner}>Show scanner</button>
-          <button onClick={this.logout}>Logout</button>
+        <div className="sidebar"></div>
+        <div className={`content tall${this.state.showScanner ? ' blur' : ''}`}>
+          <div className="row">
+            <input type="text" value={this.state.search} onChange={this.onSearchChange} placeholder="Search..."/>
+            <button className="btn" onClick={this.showScanner}>Scan</button>
+          </div>
+
+          <div className="list-group">
+            {hackers}
+          </div>
+          <button className="btn" onClick={this.logout} style={{marginTop: '20px'}}>Logout</button>
         </div>
 
         {scanner}
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from '@/store';
+import 'bootstrap/dist/css/bootstrap.css';
 import './styles.scss';
 
 import App from '@/components/App'
