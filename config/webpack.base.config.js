113d3b2eaf250a652328fcf7116d0fc7625d105a
drewthoennes <dthoenne@purdue.edu>
2019-07-17 23:55:09 -0500

    Removes unnecessary modules

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -60,10 +60,6 @@ module.exports = {
         {
           test:/\.(s*)css$/,
           use: ['style-loader', 'css-loader', 'sass-loader']
-        },
-        {
-          test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-          loader: 'url-loader?limit=100000'
         }
       ]
   },
